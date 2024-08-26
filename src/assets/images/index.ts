b835b963461f4a80fe9dbeddf24d30ff329a8442
Zeeshan6088 <shan60884@gmail.com>
2024-08-26 15:22:40 +0500

    added index files for assets

---
+++ src/assets/images/index.ts
@@ -0,0 +1,11 @@
+export const images = {
+  onbordingimg1: require('./Onbordingimg1.png'),
+  onbordingimg2: require('./Onbordingimg2.png'),
+  onbordingimg3: require('./Onbordingimg3.png'),
+  applogo: require('./Applogo.png'),
+  hideicon: require('./Hideicon.png'),
+  unhideicon: require('./Unhideicon.png'),
+  accountimg: require('./Accountimg.png'),
+  forgotimg: require('./Forgotimg.png'),
+  Passwordimg: require('./PasswordSuccessful.png'),
+};
---
+++ src/assets/svg/index.ts
@@ -0,0 +1,8 @@
+import Checkbox from './Checkbox.svg';
+import Messegeicon from './Messegeicon.svg';
+import Lockicon from './Lockicon.svg';
+import Nameicon from './Nameicon.svg';
+import Referalicon from './Referalicon.svg';
+import UnCheckbox from './UnCheckbox.svg';
+
+export {Checkbox, Messegeicon, Lockicon, Nameicon, Referalicon, UnCheckbox};
